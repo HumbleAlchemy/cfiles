dac5a41fa9bce87929b8aea0a5b22f5030f7de41
HumbleAlchemy <abhishek.alchemist@gmail.com>
2013-03-17 06:56:12 +0530

    add project skeleton

---
+++ linkedlist.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+struct node {
+	int info;
+	struct node *next;
+};
+
+//add node at first
+//add node at last
+//add new node at given position
+//delete first node
+//delete last node
+//delete given node
+//sort
+//reverse
+//concatenate two node
+
+
+int int main(int argc, const char *argv[])
+{
+	
+	int a, b;
+	printf("working!\n") ;
+	//printf("definitely\n");
+	
+		
+	return 0;
+}
